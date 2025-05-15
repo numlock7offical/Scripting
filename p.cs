b8f451995a8bba17ac90001fdac2f75e4d9e35c9
numlock7offical <shihiriny@gmail.com>
2025-05-15 11:44:02 +0500

    Add files via upload

---
+++ p.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class p : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {   //Вывод hello world! в консоль
+        print("hello world!");
+        print("hello world!");
+        print("hello world!");
+        print("hello world!");
+        print("hello world!");
+        print("hello world!");
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
